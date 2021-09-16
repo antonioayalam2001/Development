97b4a4fe19fc9a49e0d5d2c751aa1a404cbadf93
antonioayalam2001 <antonioayalam2001@gmail.com>
2021-09-15 22:30:44 -0500

    Quitando comentarios de Arbol ABB

--- Analisis_Algoritmos/Practica1/Ejecutables_Linux/ArbolABB.sh
+++ Analisis_Algoritmos/Practica1/Ejecutables_Linux/ArbolABB.sh
@@ -7,16 +7,16 @@ gcc PruebasAlgoritmo.c -lm -o PruebasAlgoritmo
 ./PruebasAlgoritmo 7 1000 >>numeros.txt
 ./PruebasAlgoritmo 7 5000 >>numeros.txt
 ./PruebasAlgoritmo 7 10000
-# ./PruebasAlgoritmo 7 50000
-# ./PruebasAlgoritmo 7 100000
-# ./PruebasAlgoritmo 7 200000
-# ./PruebasAlgoritmo 7 400000
-# ./PruebasAlgoritmo 7 600000
-# ./PruebasAlgoritmo 7 800000
-# ./PruebasAlgoritmo 7 1000000
-# ./PruebasAlgoritmo 7 2000000
-# ./PruebasAlgoritmo 7 3000000
-# ./PruebasAlgoritmo 7 4000000
+./PruebasAlgoritmo 7 50000
+./PruebasAlgoritmo 7 100000
+./PruebasAlgoritmo 7 200000
+./PruebasAlgoritmo 7 400000
+./PruebasAlgoritmo 7 600000
+./PruebasAlgoritmo 7 800000
+./PruebasAlgoritmo 7 1000000
+./PruebasAlgoritmo 7 2000000
+./PruebasAlgoritmo 7 3000000
+./PruebasAlgoritmo 7 4000000
 # ./PruebasAlgoritmo 7 5000000
 # ./PruebasAlgoritmo 7 6000000
 # ./PruebasAlgoritmo 7 7000000
