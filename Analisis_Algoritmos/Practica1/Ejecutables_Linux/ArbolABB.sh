7935a06bfcd48ee6db9542306bdb15ef1c5e4144
antonioayalam2001 <antonioayalam2001@gmail.com>
2021-09-15 22:39:00 -0500

    Realizando: Cambios

--- Analisis_Algoritmos/Practica1/Ejecutables_Linux/ArbolABB.sh
+++ Analisis_Algoritmos/Practica1/Ejecutables_Linux/ArbolABB.sh
@@ -21,7 +21,7 @@ gcc PruebasAlgoritmo.c -lm -o PruebasAlgoritmo
 ./PruebasAlgoritmo 7 6000000
 ./PruebasAlgoritmo 7 7000000
 ./PruebasAlgoritmo 7 8000000
-./PruebasAlgoritmo 7 9000000
-./PruebasAlgoritmo 7 10000000
+# ./PruebasAlgoritmo 7 9000000
+# ./PruebasAlgoritmo 7 10000000
 
 
