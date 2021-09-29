6a1f79441891e5cf5344d1ddcc02eb1e23d66eaf
antonioayalam2001 <antonioayalam2001@gmail.com>
2021-09-29 08:14:06 -0500

    EjecutableExponencial

--- Analisis_Algoritmos/Practica2/Ejecutables_Linux/Exponencial.sh
+++ Analisis_Algoritmos/Practica2/Ejecutables_Linux/Exponencial.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo Lineal Sin Hilos
+echo Exponencial
 cd ..
 gcc MainLin.c -lm -o MainLin -pthread
-./MainLin 1000000 5 >Exponencial.txt
-./MainLin 2000000 5 >>Exponencial.txt
-./MainLin 3000000 5 >>Exponencial.txt
-./MainLin 4000000 5 >>Exponencial.txt
-./MainLin 5000000 5 >>Exponencial.txt
-./MainLin 6000000 5 >>Exponencial.txt
-./MainLin 7000000 5 >>Exponencial.txt
-./MainLin 8000000 5 >>Exponencial.txt
-./MainLin 9000000 5 >>Exponencial.txt
-./MainLin 10000000 5 >>Exponencial.txt
+./MainLin 1000000 8 >Exponencial.txt
+./MainLin 2000000 8 >>Exponencial.txt
+./MainLin 3000000 8 >>Exponencial.txt
+./MainLin 4000000 8 >>Exponencial.txt
+./MainLin 5000000 8 >>Exponencial.txt
+./MainLin 6000000 8 >>Exponencial.txt
+./MainLin 7000000 8 >>Exponencial.txt
+./MainLin 8000000 8 >>Exponencial.txt
+./MainLin 9000000 8 >>Exponencial.txt
+./MainLin 10000000 8 >>Exponencial.txt
