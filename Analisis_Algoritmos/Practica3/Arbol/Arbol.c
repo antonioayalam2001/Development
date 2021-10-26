edeb545c9ba9225e77e7d875e17d26a2c0e6ce10
antonioayalam2001 <antonioayalam2001@gmail.com>
2021-10-26 11:20:19 -0500

    Quitando Practica3

--- Analisis_Algoritmos/Practica3/Arbol/Arbol.c
+++ Analisis_Algoritmos/Practica3/Arbol/Arbol.c
@@ -1,281 +1,353 @@
-// #include "Arbol.h"
+// // #include "Arbol.h"
+// // #include <stdlib.h>
+// // #include <stdio.h>
+
+// // int arrayIndex = 0;
+
+// // /*
+// //  * Función encargada de iniciar el apuntado a arbol
+// //  *
+// //  * */
+// // void Iniciar(arbol* a){
+// // 	*a = NULL;
+// // 	return;
+// // }
+
+// // /*
+// //  * Función que determina si un arbol está vacío
+// //  * Regresa 1 en caso de que esté vacío y 0 en otro caso
+// //  *
+// //  * */
+// // int Vacio(arbol* a){
+// // 	if(*a == NULL)
+// // 	{
+// // 		return 1;
+// // 	}
+// // 	else
+// // 	{
+// // 		return 0;
+// // 	}
+// // }
+
+// // /*
+// //  * Función encargada de insertar nuevos nodos
+// //  * al arbol, recursivamente
+// //  *
+// //  * */
+// // void NuevoNodo(arbol* a, int valor){
+// // 	// Si el árbol está vacío, crea un nodo en la raíz
+// // 	if(Vacio(a) == 1)
+// // 	{
+// // 		*a = CreaNodo(valor);
+// // 	}
+// // 	else
+// // 	{
+// // 		// Determinamos hacia donde hay que mover
+// // 		// la posición
+// // 		if((*a)->valor < valor)
+// // 		{
+// // 			// Llamamos nuevamente a esta función
+// // 			// pero ahora con la nueva ubicación
+// // 			NuevoNodo(&((*a)->derecho),valor);
+// // 		}
+// // 		else
+// // 		{
+// // 			// Llamamos nuevamente a esta función
+// // 			// pero ahora con la nueva ubicación
+// // 			NuevoNodo(&((*a)->izquierdo),valor);
+// // 		}
+// // 	}
+// // 	return;
+// // }
+
+// // /*
+// //  * Función encargada de crear un nuevo nodo con el valor
+// //  * enviado en el parámetro
+// //  *
+// //  * */
+// // Nodo * CreaNodo(int valor){
+// // 	Nodo * aux = (Nodo *)malloc(sizeof(Nodo));
+// // 	aux->valor = valor;
+// // 	aux->derecho = NULL;
+// // 	aux->izquierdo = NULL;
+// // 	return aux;
+// // }
+
+// // /*
+// //  * Función encargada de rellenar un arreglo mediante
+// //  * la lectura InOrder del arbol
+// //  *
+// //  * */
+// // void RellenaInorden(arbol* a, int * arreglo)
+// // {
+// // 	// Comprobamos que no esté vacío
+// // 	if(Vacio(a) != 1)
+// // 	{
+// // 		// Nos movemos a la izquierda y llamamos a la función nuevamente
+// // 		RellenaInorden(&((*a)->izquierdo),arreglo);
+// // 		// Aumentamos el índice y almacenamos el valor en el arreglo
+// // 		arreglo[arrayIndex++] = (*a)->valor;
+// // 		// Nos movemos a la derecha y llamamos a la función nuevamente
+// // 		RellenaInorden(&((*a)->derecho),arreglo);
+// // 	}
+// // 	return;
+// // }
+
+// // /*
+// //  * Función que busca iterativamente un valor dentro
+// //  * de un árbol dado
+// //  * Parámetros:
+// //  *	a - el árbol en el que se desea buscar
+// //  *	valor - el valor a buscar dentro del árbol
+// //  *	encontrado - nos indicará si el valor fue encontrado
+// //  * */
+// // void BuscaValor(arbol* a, int valor, int *encontrado)
+// // {
+// // 	// Empleamos un auxiliar para no perder la raíz
+// // 	// de nuestro árbol
+// // 	arbol * sub = a;
+// // 	int x = 0;
+// // 	// Mientras existan nodos y no se haya encontrado el valor
+// // 	while(Vacio(sub) != 1 && *encontrado == -1)
+// // 	{
+// // 		// Si el nodo en el que estamos contiene el valor
+// // 		// significa que fue encontrado
+// // 		if((*sub)->valor == valor)
+// // 		{
+// // 			*encontrado = valor;
+// //             printf("Si encontramos el valor we en %d",valor);
+// // 			break;
+// // 		}
+// // 		else if((*sub)->valor < valor)
+// // 		{
+// // 			// Si el valor que buscamos es mayor
+// // 			// al del nodo en el que nos encontramos
+// // 			// nos movemos a la derecha
+// // 			sub = &((*sub)->derecho);
+// // 		}
+// // 		else if((*sub)->valor > valor)
+// // 		{
+// // 			// Si el valor que buscamos es menor
+// // 			// al del nodo en el que nos encontramos
+// // 			// nos movemos a la izquierda
+// // 			sub = &((*sub)->izquierdo);
+// // 		}
+// // 	}
+// // 	return;
+// // }
+
+// //// Arbol Mio
+// // Arbol de Busqueda Binaria
+// // Analisis de Algoritmos
+// // Autores: Mora Ayala Jose, Antonio,
+// // 3CM12
 // #include <stdlib.h>
-// #include <stdio.h>
+// #include "Arbol.h"
 
-// int arrayIndex = 0;
+// // *A = Arreglo de lista de numeros
+// // n Longitud del arreglo
+// // DatoRecibido = Numeros de la lista obtenidos mediante el for
 
-// /*
-//  * Función encargada de iniciar el apuntado a arbol
-//  *
-//  * */
-// void Iniciar(arbol* a){
-// 	*a = NULL;
-// 	return;
+// int *InsertarABB(int *A, int n, int valorABuscar, int *aviso)
+// {
+// 	// Declarando raiz del arbol
+// 	arbolBinario *Raiz;
+// 	//Reservando memoria
+// 	Raiz = (arbolBinario *)malloc(sizeof(arbolBinario));
+// 	// Estableciendo la raiz como vacia
+// 	Raiz = NULL;
+// 	for (int i = 0; i < n; ++i)
+// 		// Recorriendo la lista para ir insertando cada numero en el arbol
+// 		Insertar(&Raiz, A[i]);
+
+// 	// A = Inorden(Raiz,A);
+// 	//- BusquedaArbol(Raiz,902);
+// 	BusquedaArbol(Raiz, valorABuscar, aviso);
 // }
 
-// /*
-//  * Función que determina si un arbol está vacío
-//  * Regresa 1 en caso de que esté vacío y 0 en otro caso
-//  *
-//  * */
-// int Vacio(arbol* a){
-// 	if(*a == NULL)
-// 	{
-// 		return 1;
-// 	}
-// 	else 
-// 	{
-// 		return 0;
-// 	}
-// }
-
-// /*
-//  * Función encargada de insertar nuevos nodos
-//  * al arbol, recursivamente
-//  *
-//  * */
-// void NuevoNodo(arbol* a, int valor){
-// 	// Si el árbol está vacío, crea un nodo en la raíz
-// 	if(Vacio(a) == 1)
-// 	{
-// 		*a = CreaNodo(valor);
-// 	}
+// void Insertar(arbolBinario **Raiz, int DatoRecibido)
+// {
+// 	//Se crea un nodo auxiliar
+// 	arbolBinario **Auxiliar = NULL;
+// 	//Se iguala el auxiliar numeros la raiz
+// 	Auxiliar = Raiz;
+// 	// Si no hay dato se crea un nodo nuevo
+// 	if (*Raiz == NULL)
+// 		*Raiz = InicializarArbol(DatoRecibido);
 // 	else
 // 	{
-// 		// Determinamos hacia donde hay que mover
-// 		// la posición
-// 		if((*a)->valor < valor)
+
+// 		// Bucle para buscar un nodo que este vacio y poder proceder a una
+// 		// nueva insercion de dato
+// 		while (*Auxiliar != NULL)
 // 		{
-// 			// Llamamos nuevamente a esta función
-// 			// pero ahora con la nueva ubicación
-// 			NuevoNodo(&((*a)->derecho),valor);
+// 			// Si el numero a ingresar es menor en Auxiliar
+// 			if (DatoRecibido < (*Auxiliar)->dato)
+// 				// Se va al nodo izquierdo
+// 				Auxiliar = &((*Auxiliar)->izq);
+// 			//Si el numero a ingresar es mayor en Auxiliar
+// 			else if (DatoRecibido > (*Auxiliar)->dato)
+// 				Auxiliar = &((*Auxiliar)->der); //Se va al nodo derecho
 // 		}
-// 		else	
+// 		// Ya se llego a la posicion adecuada, agrega el nuevo nodo
+// 		// Cuando llegamos a la posicion donde no había dato podemos agregar
+// 		// el nodo nuevo (Auxiliar ya sabe si debe ir a la izquierda o derecha)
+// 		// Gracias al if que pusimos
+// 		*Auxiliar = InicializarArbol(DatoRecibido);
+// 	}
+// }
+
+// /*
+
+// Funcion: Recibe un arbol binario Raiz y una posicion A, realiza el recorrido izquierdo -> raiz -> derecho a partir de la posicion A.
+// Requerimientos: El arbol binario A es no vacio y la posicion A es una posicion valida.
+// */
+// int * Inorden (arbolBinario * Raiz, int *A)
+// {
+// 	int i = 0;
+// 	arbolBinario *Auxiliar,*Recorrido;
+// 	Auxiliar = Raiz;
+// 	while (Auxiliar !=NULL)
+// 	{
+// 		if (Auxiliar->izq == NULL)
 // 		{
-// 			// Llamamos nuevamente a esta función
-// 			// pero ahora con la nueva ubicación
-// 			NuevoNodo(&((*a)->izquierdo),valor);
+// 			A[i] = Auxiliar->dato;
+// 			Auxiliar = Auxiliar->der;
+// 			i++;
+// 		}
+// 		else
+// 		{
+// 			Recorrido = Auxiliar->izq;
+// 			while(Recorrido->der !=NULL && Recorrido->der != Auxiliar)
+// 				Recorrido = Recorrido->der;
+// 			if(Recorrido->der == NULL)
+// 			{
+// 				Recorrido->der = Auxiliar;
+// 				Auxiliar = Auxiliar->izq;
+// 			}
+// 			else
+// 			{
+// 				Recorrido->der = NULL;
+// 				A[i] = Auxiliar->dato;
+// 				Auxiliar = Auxiliar->der;
+// 				i++;
+// 			}
 // 		}
 // 	}
-// 	return;
+// 	return A;
 // }
 
-// /*
-//  * Función encargada de crear un nuevo nodo con el valor
-//  * enviado en el parámetro
-//  *
-//  * */
-// Nodo * CreaNodo(int valor){
-// 	Nodo * aux = (Nodo *)malloc(sizeof(Nodo));
-// 	aux->valor = valor;
-// 	aux->derecho = NULL;
-// 	aux->izquierdo = NULL;
-// 	return aux;
-// }
+// int BusquedaArbol(arbolBinario *Raiz, int valor,int *aviso){
+// 	arbolBinario *aux = Raiz;
+// 	int x=0;
+// 			// printf("Ya fue encontrado");
 
-// /*
-//  * Función encargada de rellenar un arreglo mediante 
-//  * la lectura InOrder del arbol
-//  *
-//  * */
-// void RellenaInorden(arbol* a, int * arreglo)
-// {
-// 	// Comprobamos que no esté vacío
-// 	if(Vacio(a) != 1)
+// 	while (aux!=NULL)
 // 	{
-// 		// Nos movemos a la izquierda y llamamos a la función nuevamente
-// 		RellenaInorden(&((*a)->izquierdo),arreglo);
-// 		// Aumentamos el índice y almacenamos el valor en el arreglo
-// 		arreglo[arrayIndex++] = (*a)->valor;
-// 		// Nos movemos a la derecha y llamamos a la función nuevamente
-// 		RellenaInorden(&((*a)->derecho),arreglo);		
-// 	}
-// 	return;
-// }
-
-// /*
-//  * Función que busca iterativamente un valor dentro
-//  * de un árbol dado
-//  * Parámetros:
-//  *	a - el árbol en el que se desea buscar
-//  *	valor - el valor a buscar dentro del árbol
-//  *	encontrado - nos indicará si el valor fue encontrado
-//  * */
-// void BuscaValor(arbol* a, int valor, int *encontrado)
-// {
-// 	// Empleamos un auxiliar para no perder la raíz
-// 	// de nuestro árbol
-// 	arbol * sub = a;
-// 	int x = 0;
-// 	// Mientras existan nodos y no se haya encontrado el valor
-// 	while(Vacio(sub) != 1 && *encontrado == -1)
-// 	{
-// 		// Si el nodo en el que estamos contiene el valor
-// 		// significa que fue encontrado
-// 		if((*sub)->valor == valor)
+// 		if ((aux->dato) == valor)
 // 		{
-// 			*encontrado = valor;
-//             printf("Si encontramos el valor we en %d",valor);
+// 			*aviso=1;
 // 			break;
 // 		}
-// 		else if((*sub)->valor < valor)
-// 		{
-// 			// Si el valor que buscamos es mayor
-// 			// al del nodo en el que nos encontramos
-// 			// nos movemos a la derecha
-// 			sub = &((*sub)->derecho);
-// 		}		
-// 		else if((*sub)->valor > valor)
-// 		{
-// 			// Si el valor que buscamos es menor
-// 			// al del nodo en el que nos encontramos
-// 			// nos movemos a la izquierda
-// 			sub = &((*sub)->izquierdo);
-// 		}	
+// 		else if ((aux->dato) < valor){
+// 			aux=((aux->der));
+// 		}
+// 		else if ((aux->dato) > valor){
+// 			aux=((aux->izq));
+// 		}
+
 // 	}
-// 	return;
+
 // }
 
-//// Arbol Mio
-// Arbol de Busqueda Binaria
-// Analisis de Algoritmos
-// Autores: Mora Ayala Jose, Antonio, 
-// 3CM12
+// // Inicializa un arbol, reservando memoria y asignando primer elemento a Dato
+// // 									Recibe un dato
+// // arbolbinario * IniciarlizarAtblo (int NumDato)
+// arbolBinario * InicializarArbol (int numDato)
+// {
+// 	arbolBinario *A = malloc(sizeof(nodo));
+// 	// Asignando el DatoRecibido a dato del aebol
+// 	A->dato= numDato;
+// 	//Sin nodo izquierdo
+// 	A->izq=NULL;
+// 	//Sin nodo derecho
+// 	A->der=NULL;
+// 	// Regresamos el Arreglo
+// 	return A;
+// }
+
+#include <stdio.h>
 #include <stdlib.h>
-#include "Arbol.h"
+#define MAXCANDIDATOS 100 // max cantidad de candidatos
+#define NMAX 20			  // máximo tamaño del tablero
+#define TRUE 1
+#define FALSE 0
+int nroSol; // cuenta soluciones
+void imprimeSolucion(int a[], int n);
+int construyeCandidatos(int a[], int k, int n, int c[]);
+void backtrack(int a[], int k, int n);
 
-// *A = Arreglo de lista de numeros 
-// n Longitud del arreglo
-// DatoRecibido = Numeros de la lista obtenidos mediante el for
-
-int * InsertarABB (int * A, int n,int valorABuscar,int *aviso)
+	void main(void)
 {
-	// Declarando raiz del arbol
-	arbolBinario *Raiz;
-	//Reservando memoria
-	Raiz = (arbolBinario *) malloc (sizeof(arbolBinario));
-	// Estableciendo la raiz como vacia 
-	Raiz = NULL;
-	for (int i = 0; i < n; ++i)
-	// Recorriendo la lista para ir insertando cada numero en el arbol
-		Insertar(&Raiz,A[i]);
-		
-	// A = Inorden(Raiz,A);
-	//- BusquedaArbol(Raiz,902);
-	BusquedaArbol(Raiz,valorABuscar,aviso);
+	int a[NMAX], n;
+	printf("Tamano del tablero: ");
+	scanf(" %d", &n);
+	nroSol = 0;
+	backtrack(a, 0, n);
+	printf("Nro Soluciones %d \n", nroSol);
 }
 
-void Insertar(arbolBinario **Raiz,int DatoRecibido)
+void imprimeSolucion(int a[], int n)
 {
-	//Se crea un nodo auxiliar
-	arbolBinario **Auxiliar = NULL; 
-	 //Se iguala el auxiliar numeros la raiz
-	Auxiliar = Raiz;
-	// Si no hay dato se crea un nodo nuevo
-	if (*Raiz == NULL) 
-		*Raiz = InicializarArbol(DatoRecibido);
+	int i, j;
+	nroSol++;
+	printf("Solucion: % d\n", nroSol);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			if (i == a[j])
+				printf("R");
+			else
+				printf("-");
+		printf("\n");
+	}
+	printf("== == == == == == == == ==\n");
+}
+
+int construyeCandidatos(int a[], int k, int n, int c[])
+{
+	int i, j, ncandidatos = 0;
+	for (i = 0; i < n; i++)
+	{
+		int band = TRUE;
+		for (j = 0; j < k; j++)
+		{
+			/* restriccion de diagonal restriccion de fila */
+			if (abs(k - j) == abs(i - a[j]) || i == a[j])
+				band = FALSE;
+		}
+		if (band)
+		{
+			c[ncandidatos] = i;
+			ncandidatos++;
+		}
+	}
+	return ncandidatos;
+}
+
+void backtrack(int a[], int k, int n)
+{
+	int i;
+	int c[MAXCANDIDATOS];
+	if (k == n)
+		imprimeSolucion(a, k);
 	else
 	{
-		
-		// Bucle para buscar un nodo que este vacio y poder proceder a una
-		// nueva insercion de dato
-		while(*Auxiliar != NULL) 
+		int ncandidates = construyeCandidatos(a, k, n, c);
+		for (i = 0; i < ncandidates; i++)
 		{
-			// Si el numero a ingresar es menor en Auxiliar
-			if (DatoRecibido < (*Auxiliar)->dato)
-			// Se va al nodo izquierdo 
-				Auxiliar = &((*Auxiliar)->izq); 
-				//Si el numero a ingresar es mayor en Auxiliar
-			else if (DatoRecibido > (*Auxiliar)->dato) 
-				Auxiliar = &((*Auxiliar)->der); //Se va al nodo derecho
-		}
-		// Ya se llego a la posicion adecuada, agrega el nuevo nodo
-		// Cuando llegamos a la posicion donde no había dato podemos agregar 
-		// el nodo nuevo (Auxiliar ya sabe si debe ir a la izquierda o derecha)
-		// Gracias al if que pusimos
-		*Auxiliar = InicializarArbol (DatoRecibido);
-	}
-}
-
-
-/*
-
-Funcion: Recibe un arbol binario Raiz y una posicion A, realiza el recorrido izquierdo -> raiz -> derecho a partir de la posicion A.
-Requerimientos: El arbol binario A es no vacio y la posicion A es una posicion valida.
-*/
-int * Inorden (arbolBinario * Raiz, int *A)
-{
-	int i = 0;
-	arbolBinario *Auxiliar,*Recorrido;
-	Auxiliar = Raiz;
-	while (Auxiliar !=NULL)
-	{
-		if (Auxiliar->izq == NULL)
-		{
-			A[i] = Auxiliar->dato;
-			Auxiliar = Auxiliar->der;
-			i++;
-		}
-		else
-		{
-			Recorrido = Auxiliar->izq;
-			while(Recorrido->der !=NULL && Recorrido->der != Auxiliar)
-				Recorrido = Recorrido->der;
-			if(Recorrido->der == NULL)
-			{
-				Recorrido->der = Auxiliar;
-				Auxiliar = Auxiliar->izq;
-			}
-			else
-			{
-				Recorrido->der = NULL;
-				A[i] = Auxiliar->dato;
-				Auxiliar = Auxiliar->der;
-				i++;
-			}
+			a[k] = c[i];
+			backtrack(a, k + 1, n);
 		}
 	}
-	return A;
-}
-
-int BusquedaArbol(arbolBinario *Raiz, int valor,int *aviso){
-	arbolBinario *aux = Raiz;
-	int x=0;
-			// printf("Ya fue encontrado");
-
-	while (aux!=NULL)
-	{
-		if ((aux->dato) == valor)
-		{
-			*aviso=1;
-			break;
-		}
-		else if ((aux->dato) < valor){
-			aux=((aux->der));
-		}
-		else if ((aux->dato) > valor){
-			aux=((aux->izq));
-		}
-		
-	}
-	
-
-
-}
-
-
-// Inicializa un arbol, reservando memoria y asignando primer elemento a Dato
-// 									Recibe un dato
-// arbolbinario * IniciarlizarAtblo (int NumDato)
-arbolBinario * InicializarArbol (int numDato)
-{
-	arbolBinario *A = malloc(sizeof(nodo)); 
-	// Asignando el DatoRecibido a dato del aebol
-	A->dato= numDato;
-	//Sin nodo izquierdo
-	A->izq=NULL; 
-	//Sin nodo derecho 
-	A->der=NULL; 
-	// Regresamos el Arreglo
-	return A;
 }
