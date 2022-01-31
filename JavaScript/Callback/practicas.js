7888617ea01a0dbc7524540729ad4318a9c06fbe
antonioayalam2001 <antonioayalam2001@gmail.com>
2022-01-31 09:29:54 -0600

    AgregandoApuntesRedes

--- JavaScript/Callback/practicas.js
+++ JavaScript/Callback/practicas.js
@@ -82,10 +82,6 @@ const data = [
 
 const personasCreadas = [];
 
-for (let index in data) {
-  personasCreadas[index] = new Persona(data[index][0], data[index][1]);
-}
-
 const obtenerPersona = (id) => {
   return new Promise((resolve, reject) => {
     if (personasCreadas[id] == undefined) {
@@ -105,16 +101,189 @@ const obtenerInstagram = (id) => {
   });
 };
 
-obtenerPersona(1).then((persona) => {
-  console.log(persona);
-  obtenerInstagram(10)
-    .then((instagram) => {
-      console.log(instagram);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+// obtenerPersona(1).then((persona) => {
+//   console.log(persona);
+//   obtenerInstagram(10)
+//     .then((instagram) => {
+//       console.log(instagram);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// obtenerPersona(1).then((persona) => {
+//     console.log(persona);
+//     return obtenerInstagram(10)
+// }).then((instagram) => {
+//     console.log(instagram);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+// const object = {
+//     propiedad1: 'v1' ,
+//     propiedad2: 'v2',
+//     propiedad3: 'v3'
+// }
+
+// const obtenerInformacion = (text) => {
+//     return new Promise((resolve, reject) => {
+//         resolve(text)
+//     })
+// }
+
+// async function getData() {
+//   return Promise.resolve('data');
+// }
+
+// async function getAll() {
+//   const data = await obtenerInformacion('0');
+//   const data1 = await obtenerInformacion('1');
+//   const data2 = await obtenerInformacion('2');
+//   console.log(data);
+//   console.log(data1);
+//   console.log(data2);
+// }
+
+// getAll().then((all) => {
+// //   console.log(all)
+// })
+
+const materiasHTML = document.querySelector(".materias");
+const trigger = document.getElementById("trigger");
+let materia;
+const materias = [
+  {
+    nombre: "Fisica",
+    nota: 7,
+  },
+  {
+    nombre: "quimica",
+    nota: 10,
+  },
+  {
+    nombre: "Mate",
+    nota: 5,
+  },
+  {
+    nombre: "Calculo",
+    nota: 9,
+  },
+  {
+    nombre: "discretas",
+    nota: 8,
+  },
+  {
+    nombre: "Programacion",
+    nota: 4,
+  },
+];
+
+materias;
+
+const obtenerMateria = (id) => {
+  return new Promise((resolve, reject) => {
+    materia = materias[id];
+    if (materia == undefined) {
+      reject("La materia no existe");
+    } else {
+      setTimeout(() => {
+        resolve(materia);
+      }, Math.random() * 400);
+    }
+  });
+};
+
+// obtenerMateria(1).then(res=>console.log(res))
+// obtenerMateria(2).then(res=>console.log(res))
+// obtenerMateria(3).then(res=>console.log(res))
+// obtenerMateria(4).then(res=>console.log(res))
+// obtenerMateria(5).then(res=>console.log(res))
+// obtenerMateria(6).then(res => console.log(res)).catch((err) => console.log(err))
+
+const escribirHola = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        (materiasHTML.innerHTML += `<h1 style="color:powderblue; padding:12px;"> Tu listado de materias es el siguiente</h1>`)
+      );
+    }, 1000);
+  });
+};
+
+const devolverMaterias = async () => {
+  trigger.disabled = true;
+  trigger.remove();
+  let materia = [];
+  await escribirHola();
+  for (let i = 0; i < materias.length; i++) {
+    materia[i] = await obtenerMateria(i);
+    let newHtmlCode = `<div class="materia">
+        <div class="nombre">${materia[i].nombre}</div>
+        <div class="nota">${materia[i].nota}</div>
+      </div>`;
+    materiasHTML.innerHTML += newHtmlCode;
+  }
+};
+
+trigger.addEventListener("click", devolverMaterias);
+
+let objeto = [
+  {
+    saludo: "hola",
+    saludo2: "queonda",
+  },
+];
+// console.log(typeof(JSON.stringify(objeto)));
+const JSONserializado = '{saudo:"hola", saludo2:"adios"}';
+
+const peticion = new XMLHttpRequest();
+peticion.addEventListener("load", () => {
+  if (peticion.readyState == 4 && peticion.status == 200) {
+    let respuesta, objeto;
+    respuesta = peticion.response;
+    objeto = JSON.parse(respuesta);
+    console.log(objeto.edad);
+    // console.log(JSON.parse(respuesta).edad);
+  } else {
+    console.log("Error ");
+  }
 });
+
+peticion.open("GET", "json.txt");
+peticion.send();
+
+// if (window.XMLHttpRequest) {
+//   let peticion1 = new XMLHttpRequest
+// } else {
+//   let peticion1 = new ActiveXObject('Microsoft.XMLHTTP')
+// }
+const displayEmail = document.getElementById("display");
+let dataJ;
+peticionFetch = fetch("https://reqres.in/api/users?page=2");
+let datas = [];
+peticionFetch.then((res) => {
+  res.text().then((res) => {
+    dataJ = JSON.parse(res);
+    for (const index in dataJ.data) {
+      console.log(dataJ.data[index].email);
+      datas.push(dataJ.data[index].email);
+      displayEmail.innerHTML += `${dataJ.data[index].email} <br>`;
+    }
+  });
+});
+
+fetch("https://reqres.in/api/users")
+  .then((res) => res.json())
+  .then((res) => console.log(res));
+
+
+axios.post("https://reqres.in/api/users", {
+  "nombre": "Tony",
+  "Apellido":"Ayala"
+}).then(res => console.log(res))
+
