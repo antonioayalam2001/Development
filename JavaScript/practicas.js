35a6903a405148c4c74e953d399c3853f412d93e
antonioayalam2001 <antonioayalam2001@gmail.com>
2022-02-16 16:34:32 -0600

    Nuevo Proyecto

--- JavaScript/practicas.js
+++ JavaScript/practicas.js
@@ -486,17 +486,17 @@
     
 
 
-// const materias = {
-// 	fisica: [90,6,3,"fisica"],
-// 	matematica: [84,8,2,"matematica"],
-// 	logica: [92,8,4,"logica"],
-// 	quimica: [96,8,4,"quimica"],
-// 	calculo: [91,6,3,"calculo"],
-// 	programacion: [79,7,4,"programacion"],
-// 	biologia: [75,9,2,"biologia"],
-// 	bbdd: [98,9,1,"bbdd"],
-// 	algebra: [100,10,4,"algebra"]
-// }
+const materias = {
+	fisica: [90,6,3,"fisica"],
+	matematica: [84,8,2,"matematica"],
+	logica: [92,8,4,"logica"],
+	quimica: [96,8,4,"quimica"],
+	calculo: [91,6,3,"calculo"],
+	programacion: [79,7,4,"programacion"],
+	biologia: [75,9,2,"biologia"],
+	bbdd: [98,9,1,"bbdd"],
+	algebra: [100,10,4,"algebra"]
+}
 
 
 // const aprobo = ()=>{
