867cab35ac91cfd7479a93a4b83b4ce115225d4c
antonioayalam2001 <antonioayalam2001@gmail.com>
2022-06-29 11:39:43 -0500

    Plantilla completa

--- NodeJs/05TemplateEngines/server.js
+++ NodeJs/05TemplateEngines/server.js
@@ -4,15 +4,3 @@ let app = require('./app'),
 server = app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
 })
-
-// const array = [1, 2, 3, 4, 5]
-// const [numero1, numero2] = array
-// console.log(numero1, numero2);
-
-// const objeto = {
-//    nombre: 'Juan',
-//    apellido: 'Perez'
-// }
-
-// const { nombre, apellido } = objeto
-// console.log(nombre, apellido);
