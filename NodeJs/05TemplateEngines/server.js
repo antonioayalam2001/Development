24066b9621aba807a385abceee28c9728d53dc3f
antonioayalam2001 <antonioayalam2001@gmail.com>
2022-06-29 11:52:29 -0500

    Cambio

--- NodeJs/05TemplateEngines/server.js
+++ NodeJs/05TemplateEngines/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Esta es la aplicación principal que controla todo
 let app = require('./app'),
 server = app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
