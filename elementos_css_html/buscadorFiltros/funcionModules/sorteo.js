fa6d48b727e742b54e3950a302dd9b72bb8c2f53
antonioayalam2001 <antonioayalam2001@gmail.com>
2022-07-11 11:17:05 -0500

    Manejo del DOM

--- elementos_css_html/buscadorFiltros/funcionModules/sorteo.js
+++ elementos_css_html/buscadorFiltros/funcionModules/sorteo.js
@@ -7,9 +7,9 @@ export default function winner(button,selectors) {
             array.push(li.innerHTML)
         })
 
-        let number = Math.round((Math.random()* (array.length))-1)
+        let number = Math.round((Math.random()* (array.length)))
 
-        Swal.fire(`You got a Winner \n  ${array[number]}`
+        Swal.fire(`You got a Winner \n  ${array[number == array.length ? number-1 : number]}`
         )
     })
 }
